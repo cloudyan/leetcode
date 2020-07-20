eef1e35b5a61e8e336a8d5aaac1b88193ddb8fdd
cloudyan <1395093509@qq.com>
2020-07-20 16:45:30 +0800

    feat: add 70 83 88

---
+++ src/70.爬楼梯.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// f(x)=f(x−1)+f(x−2) 兔子数列
+// 动态规划
+var climbStairs = function(n) {
+  let p = 0
+  let q = 0
+  let r = 1
+  for (let i = 1; i <= n; ++i) {
+    p = q;
+    q = r;
+    r = p + q;
+  }
+  return r;
+};
+// @lc code=end
+
