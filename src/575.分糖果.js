9b5f7dab9e914969c7792743e71f3e54e1314de6
cloudyan <1395093509@qq.com>
2020-08-05 11:30:58 +0800

    feat: update 575

--- src/575.分糖果.js
+++ src/575.分糖果.js
@@ -9,24 +9,33 @@
  * @param {number[]} candies
  * @return {number}
  */
+// var distributeCandies = function(candies) {
+//   const cache = {}
+
+//   const len = candies.length
+//   const max = len/2
+//   let num = 0;
+//   for (let i = 0; i < len; i++) {
+//     const temp = candies[i]
+//     if (!cache[temp]) {
+//       num++
+//       cache[temp] = true
+//       if (num == max) return num
+//     } else {
+//       continue
+//     }
+//   }
+
+//   return num;
+// };
+
 var distributeCandies = function(candies) {
-  const cache = {}
-
-  const len = candies.length
-  const max = len/2
-  let num = 0;
-  for (let i = 0; i < len; i++) {
-    const temp = candies[i]
-    if (!cache[temp]) {
-      num++
-      cache[temp] = true
-      if (num == max) return num
-    } else {
-      continue
-    }
-  }
-
-  return num;
+  const types = new Set(candies)
+  const max = candies.length/2
+  const len = types.size
+  // console.log(len, max)
+  return len < max ? len : max;
 };
+
 // @lc code=end
 
