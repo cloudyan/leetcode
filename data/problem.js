c19d9e8b0f70281e430e999f3b3628da7c4a125d
cloudyan <1395093509@qq.com>
2020-08-05 11:11:15 +0800

    feat: add 383

---
+++ data/problem.js
@@ -0,0 +1,21 @@
+{
+  "data": {
+    "translations": [
+      {
+        "questionId": "1",
+        "title": "\u4e24\u6570\u4e4b\u548c",
+        "__typename": "AppliedTranslationNode"
+      },
+      {
+        "questionId": "2",
+        "title": "\u4e24\u6570\u76f8\u52a0",
+        "__typename": "AppliedTranslationNode"
+      },
+      {
+        "questionId": "3",
+        "title": "\u65e0\u91cd\u590d\u5b57\u7b26\u7684\u6700\u957f\u5b50\u4e32",
+        "__typename": "AppliedTranslationNode"
+      }
+    ]
+  }
+}
