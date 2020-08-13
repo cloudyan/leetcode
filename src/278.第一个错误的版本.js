ff6ca94d3a028071dcc75ee8763eca9c78cd77c9
cloudyan <1395093509@qq.com>
2020-08-13 15:51:41 +0800

    feat: add format config

--- src/278.第一个错误的版本.js
+++ src/278.第一个错误的版本.js
@@ -22,27 +22,27 @@
 
 // 最简单方法，从第一个版本遍历向后找，直到找到第一个错误的版本(超时)
 // 提高效率，二分法？
-var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        let low = 1;
-        let high = n;
-        let firstBadVer = n
-        while (low <= high) {
-            // let mid = Math.ceil((high+low)/2)
-            let mid = (high+low) >>> 1
-            if (isBadVersion(mid)) {
-                firstBadVer = mid
-                high = mid - 1
-            } else {
-                low = mid + 1
-            }
-        }
-        return firstBadVer
-    };
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let low = 1;
+    let high = n;
+    let firstBadVer = n;
+    while (low <= high) {
+      // let mid = Math.ceil((high+low)/2)
+      let mid = (high + low) >>> 1;
+      if (isBadVersion(mid)) {
+        firstBadVer = mid;
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return firstBadVer;
+  };
 };
 
 // var solution = function(isBadVersion) {
@@ -60,8 +60,4 @@ var solution = function(isBadVersion) {
 //   };
 // };
 
-
-
-
 // @lc code=end
-
